cbee165240fc05117d9eb9dcd7e180b37b06dabb
phuc50centimeters <186391026+phuc50cm@users.noreply.github.com.>
2025-03-31 12:24:05 +0700

    Serve image from my server

--- main.go
+++ main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"net/http"
+	"log"
 )
 
 func main() {
-	serveMux := http.NewServeMux()
-	httpServer := http.Server{
-		Handler: serveMux,
-		Addr: ":8080",
-	}
-	// Add handler for the root path (/)
-	serveMux.Handle("/", http.FileServer(http.Dir(".")))
+	const filepathRoot = "."
+	const port = "8080"
 
-	httpServer.ListenAndServe()
+	serveMux := http.NewServeMux()
+	// Add handler for the root path (/)
+	serveMux.Handle("/", http.FileServer(http.Dir(filepathRoot)))
+
+	httpServer := &http.Server{
+		Handler: serveMux,
+		Addr: ":" + port,
+	}
+
+
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(httpServer.ListenAndServe())
 }
